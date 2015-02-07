437a6bf74043d2222e82cfff771742b0bef9ccb1
Bruce Alderson <bruce@napkinware.com>
2015-02-07 14:59:16 -0800

    Makes base theme more generic.

--- functions.php
+++ functions.php
@@ -15,15 +15,15 @@ update_option('home', NWHOST);
 add_theme_support( 'post-thumbnails' );
 
 function nw_custom_scripts() {
-	wp_enqueue_script( 'the-3', get_template_directory_uri() . '/js/the-3.js', array( 'jquery' ), '1.0.0', true );
+	wp_enqueue_script( 'the-3', get_template_directory_uri() . '/js/site.js', array( 'jquery' ), '1.0.0', true );
 }
 
 function nw_custom_styles() {
 	if ( ! is_admin() ) {
 		 wp_enqueue_style(
 			'screen',
-			get_stylesheet_directory_uri() . '/css/the-3.less',
-			'the-3.css',
+			get_stylesheet_directory_uri() . '/css/site.less',
+			'site.css',
 			'1',
 			'screen'
 		);
