82cd20fddc17a4bd81011df26087c644e0096010
Bruce Alderson <mx@warpedvisions.org>
2021-03-26 15:47:55 -0700

    Removes unused scripts.

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if (wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-
         <link href="/favicon.ico" rel="shortcut icon">
         <link href="/touch.png" rel="apple-touch-icon-precomposed">
 
@@ -16,12 +14,12 @@
 		<meta NAME="ROBOTS" CONTENT="NOODP">
 		<link rel="author" href="https://twitter.com/robotpony" />
 		<link rel="canonical" href="https://warpedvisions.org" />
-		
+
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,700i|Work+Sans:100,400,500,700" rel="stylesheet">
 
 		<?php wp_head(); ?>
 
-<?php include_once('_parts/ga.php'); ?>
+<?php /* include_once('_parts/ga.php'); */ ?>
 
 	</head>
 <?php flush(); ?>
@@ -29,7 +27,7 @@
 
 	<header>
 		<div class="flourish left"><hr><hr><hr><hr></div>
-		<div class="flourish right"><hr><hr><hr><hr></div>		
+		<div class="flourish right"><hr><hr><hr><hr></div>
 		<div>
 			<h1>
 				<a href="/">warped<b>visions</b>.<em>org</em>
